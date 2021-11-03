75f9adc894f3661c59ce5336cfaf5bbde09208b5
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2021-11-04 01:14:53 +0530

    Updates

---
+++ precache-manifest.d200ac920c90af1f1a589d4056cfc0eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a868b1ed2725822b2b1a4c2ef5022942",
+    "url": "/meetings-app-frontend/index.html"
+  },
+  {
+    "revision": "826dacc38dd47e8526e5",
+    "url": "/meetings-app-frontend/static/css/2.a8a46a17.chunk.css"
+  },
+  {
+    "revision": "129f913a01bf2710100a",
+    "url": "/meetings-app-frontend/static/css/main.2d5ddf10.chunk.css"
+  },
+  {
+    "revision": "826dacc38dd47e8526e5",
+    "url": "/meetings-app-frontend/static/js/2.95b07f2c.chunk.js"
+  },
+  {
+    "revision": "755b9ddfce6b18cd8f4daa861415decf",
+    "url": "/meetings-app-frontend/static/js/2.95b07f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129f913a01bf2710100a",
+    "url": "/meetings-app-frontend/static/js/main.d8a6c502.chunk.js"
+  },
+  {
+    "revision": "b23bad561dee60f2326d",
+    "url": "/meetings-app-frontend/static/js/runtime-main.cf1f6c26.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
