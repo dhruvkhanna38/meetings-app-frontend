82215f7fe60ee72eb62d96162e3e4760078203fe
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2021-11-06 21:18:50 +0530

    yarn

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css'
 
