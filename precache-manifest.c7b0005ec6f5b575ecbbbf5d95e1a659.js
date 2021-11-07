9ad1b6a1cbd1d47517d858b3b9c58babcfd23273
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2021-11-08 03:59:51 +0530

    Updates

---
+++ precache-manifest.c7b0005ec6f5b575ecbbbf5d95e1a659.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0a70f57f476c6261c5c2f4d99a2054",
+    "url": "/meetings-app-frontend/index.html"
+  },
+  {
+    "revision": "5a285edaaa541d18f63b",
+    "url": "/meetings-app-frontend/static/css/2.cf39e5f4.chunk.css"
+  },
+  {
+    "revision": "ce075fcd6a4811cecf37",
+    "url": "/meetings-app-frontend/static/css/main.2d5ddf10.chunk.css"
+  },
+  {
+    "revision": "5a285edaaa541d18f63b",
+    "url": "/meetings-app-frontend/static/js/2.382e5514.chunk.js"
+  },
+  {
+    "revision": "9f25449a4d283459d8f6929b1a14e0d6",
+    "url": "/meetings-app-frontend/static/js/2.382e5514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce075fcd6a4811cecf37",
+    "url": "/meetings-app-frontend/static/js/main.31a7aff6.chunk.js"
+  },
+  {
+    "revision": "b23bad561dee60f2326d",
+    "url": "/meetings-app-frontend/static/js/runtime-main.cf1f6c26.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/meetings-app-frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d39bec012db2e556b8ab32de5bb2c900.js"
+  "/meetings-app-frontend/precache-manifest.c7b0005ec6f5b575ecbbbf5d95e1a659.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meetings-app-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
